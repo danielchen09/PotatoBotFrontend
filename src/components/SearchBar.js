77a18826a1beb6c4c2aa6f1e12ed38c8b9c9549f
danielchen09 <chen3189@purdue.edu>
2022-02-26 15:48:45 -0500

    #links

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,30 +14,68 @@ import { MdSearch } from 'react-icons/md'
 import {get_params} from "../utils";
 
 function onInputChange(e, setInput, setSearchResult) {
+    setInput(e.currentTarget.value);
     if (e.key === 'Enter') {
         search(e.currentTarget.value, setSearchResult);
     }
-    setInput(e.currentTarget.value);
 }
 
 function search(query, setSearchResults) {
-    // console.log(query);
-    fetch('https://www.googleapis.com/youtube/v3/search?' + get_params({
-        q: query,
-        part: 'snippet',
-        key: process.env.REACT_APP_GOOGLE_API_KEY
-    })).then(async r => {
-        let response = await r.json()
-        let res = response.items?.map(item => {
-            return {
-                id: item.id.videoId,
-                author: item.snippet.channelTitle,
-                title: item.snippet.title,
-                thumbnail: item.snippet.thumbnails.default.url
-            }
+    if (query.startsWith('https://www.youtube.com')) {
+        let url = new URLSearchParams('?' + query.split('?')[1]);
+        if (url.get('list')) {
+            fetch('GET https://www.googleapis.com/youtube/v3/playlistItems?' + get_params({
+                part: 'snippet',
+                playlistId: url.get('list'),
+                key: process.env.REACT_APP_GOOGLE_API_KEY
+            })).then(async r => {
+                let response = await r.json()
+                let res = response.items?.map(item => {
+                    return {
+                        id: item.id.videoId,
+                        author: item.snippet.channelTitle,
+                        title: item.snippet.title,
+                        thumbnail: item.snippet.thumbnails.default.url
+                    }
+                });
+                setSearchResults(res);
+            })
+        } else if (url.get('v')) {
+            fetch('https://www.googleapis.com/youtube/v3/videos?' + get_params({
+                part: 'snippet',
+                id: url.get('v'),
+                key: process.env.REACT_APP_GOOGLE_API_KEY
+            })).then(async r => {
+                let response = await r.json()
+                let res = response.items?.map(item => {
+                    return {
+                        id: item.id.videoId,
+                        author: item.snippet.channelTitle,
+                        title: item.snippet.title,
+                        thumbnail: item.snippet.thumbnails.default.url
+                    }
+                });
+                setSearchResults(res);
+            })
+        }
+    } else {
+        fetch('https://www.googleapis.com/youtube/v3/search?' + get_params({
+            q: query,
+            part: 'snippet',
+            key: process.env.REACT_APP_GOOGLE_API_KEY
+        })).then(async r => {
+            let response = await r.json()
+            let res = response.items?.map(item => {
+                return {
+                    id: item.id.videoId,
+                    author: item.snippet.channelTitle,
+                    title: item.snippet.title,
+                    thumbnail: item.snippet.thumbnails.default.url
+                }
+            });
+            setSearchResults(res);
         });
-        setSearchResults(res);
-    });
+    }
 }
 
 
